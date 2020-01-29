bc0ca59beb86394a1052ad8a8d95006bff23cd59
Colin Maudry <colin@maudry.com>
2020-01-29 09:29:40 +0100

    Remove trailing / in source directory

--- album.sh
+++ album.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-SOURCE_DIR=$1
+SOURCE_DIR="${1%/}"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ALBUM_TITLE="${SOURCE_DIR##*/}"
 DEST_DIR_NAME="${ALBUM_TITLE//[ \'\.]/-}"
 WEB_DIR="${SOURCE_DIR}/web/"
 DEST_DIR="${WEB_DIR}${DEST_DIR_NAME}"
 
-echo "$DEST_DIR"
-$SCRIPT_DIR/gallery.sh "$SOURCE_DIR"
+
+echo ""
+echo "DEST_DIR $DEST_DIR"
+echo ""
+
+$SCRIPT_DIR/gallery.sh "$SOURCE_DIR";
 $SCRIPT_DIR/c_transform.sh "$DEST_DIR"
